0109dc5a14aa6c6afe610b06a718d2435dc7227b
satish <satishmohan69@gmail.com>
2024-08-28 21:24:51 +0530

    After edit the code

--- module2/ipl_consecutive_wins.py
+++ module2/ipl_consecutive_wins.py
@@ -4,31 +4,26 @@ from selenium.webdriver.common.by import By
 import time
 
 service = Service(executable_path="C:\drivers\chromedriver-win64\chromedriver.exe")
-driver = webdriver.Chrome(service = service)
+driver = webdriver.Chrome(service=service)
 driver.get("https://www.iplt20.com/")
 driver.implicitly_wait(10)
 driver.maximize_window()
+
 driver.find_element(By.LINK_TEXT,"POINTS TABLE").click()
-time.sleep(3)
-#links_team_names=driver.find_elements(By.CSS_SELECTOR,"tr.team0")
 links_wins = driver.find_elements(By.XPATH,"//tr[@CLASS = 'team0 ng-scope']/td[12]/div[1]")
 
-#print(len(links_wins))
-
+"""this for loop is to append the match results in he form of list"""
 result = []
 for i in links_wins:
+    result.append(i.text)
 
-#    print(i.text)
-    result .append(i.text)
-#print("result:" , result)
-
-num = 0
+"""collect numbers of 5 consecutive wins"""
+count = []
 for j in range(len(result)):
     if result[j] == "W\nW\nW\nW\nW":
-        num = j
-        break
+        count.append(j)
 
-#print(num)
-
-hello_1 = driver.find_elements(By.XPATH,"//tr[@class = 'team0 ng-scope']/td[3]//h2[1]")
-print(hello_1[num].text)
+""" this for loop is for to print the final result """
+for num in count:
+    hello_1 = driver.find_elements(By.XPATH,"//tr[@class = 'team0 ng-scope']/td[3]//h2[1]")
+    print("This is the Team Five Consecutive wins:",hello_1[num].text)
